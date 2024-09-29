960b3e8f535b8a5a4e90487b41fc22059658ec00
cjlangan <connorjameslangan@gmail.com>
2024-09-29 11:44:51 -0500

    Now cancels all downloads

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 const iconEnabled = 'icons/cancel-duplicate-downloads.svg';
 const iconDisabled = 'icons/cancel-duplicate-downloads-disabled.svg';
 
-const duplicateDownloadRegex = /\(\d\d?\)\.[^.]+$/;
-
 var extensionEnabled = true;
 var lastCancelled = false;
 var cancelCount = 0;
@@ -12,7 +10,7 @@ function updateBrowserAction() {
   if (extensionEnabled) {
     browser.browserAction.setBadgeBackgroundColor({ color: lastCancelled ? 'darkred' : 'darkgreen' });
   }
-  browser.browserAction.setTitle({ title: `cancel-duplicate-downloads${extensionEnabled ? '' : '(DISABLED)'}: cancelled: ${cancelCount}, last action: ${lastCancelled ? 'CANCEL' : 'PASS'}` });
+  browser.browserAction.setTitle({ title: `cancel-all-downloads${extensionEnabled ? '' : '(DISABLED)'}: cancelled: ${cancelCount}, last action: ${lastCancelled ? 'CANCEL' : 'PASS'}` });
   browser.browserAction.setIcon({ path: extensionEnabled ? iconEnabled : iconDisabled });
 }
 
@@ -20,21 +18,19 @@ function handleDownloadCreated(item) {
   if (!extensionEnabled || item.state != 'in_progress') {
     return;
   }
-  lastCancelled = item.filename.match(duplicateDownloadRegex);
+  lastCancelled = true;  // Always cancel the download
   updateBrowserAction();
-  if (!lastCancelled) {
-    return;
-  }
+  
   browser.downloads
     .cancel(item.id)
     .then(
       function () {
         cancelCount++;
         updateBrowserAction();
-        console.log(`cancel-duplicate-downloads: Canceled ${item.filename} (${item.id})`);
+        console.log(`cancel-all-downloads: Canceled ${item.filename} (${item.id})`);
       },
       function (err) {
-        console.log(`cancel-duplicate-downloads: Could not cancel ${item.filename} (${item.id}): Error: ${err}`);
+        console.log(`cancel-all-downloads: Could not cancel ${item.filename} (${item.id}): Error: ${err}`);
       }
     );
 }
@@ -52,3 +48,4 @@ function handleIconClicked(_tab, clickData) {
 updateBrowserAction();
 browser.downloads.onCreated.addListener(handleDownloadCreated);
 browser.browserAction.onClicked.addListener(handleIconClicked);
+
